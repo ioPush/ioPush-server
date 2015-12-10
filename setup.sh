dd33aab6e944393d414510981d318fda6a18e5a0
Oliv4945 <oliv4945@gmail.com>
2015-12-10 22:43:19 +0100

    Use requirements.txt file for pip

--- setup.sh
+++ setup.sh
@@ -2,12 +2,6 @@
 
 pip install --user virtualenv
 python ~/.local/lib/python3.4/site-packages/virtualenv.py --no-site-packages ./virtualenv
-virtualenv/bin/pip install pytest
-virtualenv/bin/pip install pytest-cov
-virtualenv/bin/pip install flask
-virtualenv/bin/pip install flask-WTF
-virtualenv/bin/pip install flask-login
-virtualenv/bin/pip install flask-sqlalchemy
-virtualenv/bin/pip install sqlalchemy-migrate
+virtualenv/bin/pip install -r requirements.txt
 mkdir misc
 
