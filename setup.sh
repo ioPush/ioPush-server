f22803659497a5227beb9537801f12e264407de7
Oliv4945 <oliv4945@gmail.com>
2015-12-18 14:28:15 +0100

    Change log files handling for travis-ci.org

--- setup.sh
+++ setup.sh
@@ -3,12 +3,8 @@
 pip install --user virtualenv
 python ~/.local/lib/python3.4/site-packages/virtualenv.py --no-site-packages ./virtualenv
 virtualenv/bin/pip install -r requirements.txt
-mkdir -p misc/log
 chmod 775 misc
-chmod 775 misc/log
 ./db_create.py
 chmod 771 misc/ioPush.db
 chgrp www-data misc
-chgrp www-data misc/log
-chgrp www-data misc/ioPush.db
 
