7ea2abe358a153fedd848b89dc3fca9394761236
Oliv4945 <oliv4945@gmail.com>
2016-02-19 13:07:24 +0100

    Add a 'Test push notification' button

--- app/static/js/ioPush.js
+++ app/static/js/ioPush.js
@@ -7,3 +7,48 @@ $(document).ready(function() {
     var renameDeviceURL = $(e.relatedTarget).data('rename-device-url'); document.getElementById('renameDevice').action = renameDeviceURL; 
   });
 });
+
+
+// Send a push notification to all user's devices
+function pushTestAllDevices(url, authToken) {
+  document.getElementById("pushTestAllDevices").innerHTML = "Sending...";
+  
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: JSON.stringify({ "body": "Test message", "push" : "True", "badge": "I" }),
+    contentType: "application/json; charset=utf-8",
+    headers: { 'authentication_token' : authToken },
+    /*
+    success: function(data){
+      document.getElementById("pushTestAllDevices").innerHTML = "Send";
+    },
+    failure: function(errMsg) {
+      document.getElementById("pushTestAllDevices").innerHTML = "Fail to reach server";
+    },
+    statusCode: {
+      200: function(data) {
+        if (data == 'ok') {
+          document.getElementById("pushTestAllDevices").innerHTML = "Send !";
+        } else {
+          document.getElementById("pushTestAllDevices").innerHTML = "Fail";
+        }
+      },
+      404: function() {
+        document.getElementById("pushTestAllDevices").innerHTML = "Fail to reach the server";
+      }
+    }
+    */
+    complete: function(jqXHR, textStatus) {
+      switch (jqXHR.status) {
+        case 200:
+          if (jqXHR.responseText  == 'ok') {
+            document.getElementById("pushTestAllDevices").innerHTML = "Send !";
+            break;
+          }
+        default:
+          document.getElementById("pushTestAllDevices").innerHTML = "Failed";
+      }
+    }
+  });  
+}
