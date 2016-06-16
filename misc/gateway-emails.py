162deac4b5cff74c75b47fb432c6033f160f9afb
Oliv4945 <oliv4945@gmail.com>
2016-06-16 21:52:34 +0200

    Corrected mail parsing Was not converted in UTF-8

--- misc/gateway-emails.py
+++ misc/gateway-emails.py
@@ -18,13 +18,15 @@ class FakeSMTPServer(smtpd.SMTPServer):
         print('rcpttos :',rcpttos[0])
         key = rcpttos[0].rsplit('@', 1)[0]
         print('key :', key)
-        payload = {'body': email.header.decode_header(email.message_from_string(data).get('subject', None))[0][0], 'push': 'True'}
+        payload = {'body': email.header.decode_header(email.message_from_string(data).get('subject', None))[0][0].decode("utf-8"), 'push': 'True', 'httpcallback': 'True', 'httpcallback': 'True'}
+        print('payload :', payload)
         header = {'authentication_token': key}
         r = requests.post('http://localhost/app/api/post', data=json.dumps(payload), headers=header)
-        
-        '''
-        print('data :', data) 
+        data = r.text
+        print('data :', data)
+        print('status :', r.status_code) 
         print("             ----------------------------               --------------------")
+        '''
         b = email.message_from_string(data)
         subject = b.get('subject', None)
         if subject is not None:
