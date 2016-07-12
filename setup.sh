8a68404651df8bbb31e59b5a99851de4685a8f7e
Oliv4945 <oliv4945@gmail.com>
2016-07-12 10:55:21 +0200

    Change `misc/` rights and force use of python 3 to create virtualenv

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pip install --user virtualenv
-python ~/.local/lib/python3.4/site-packages/virtualenv.py --no-site-packages ./virtualenv
+python3 ~/.local/lib/python3.4/site-packages/virtualenv.py --no-site-packages ./virtualenv
 virtualenv/bin/pip install -r requirements.txt
-chmod 775 misc
+chmod -R 775 misc
 ./db_create.py
-chmod 771 misc/ioPush.db
-chgrp www-data misc
+chmod -R 771 misc/ioPush.db
+chgrp -R www-data misc
 
